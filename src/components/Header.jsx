9a077934832e7bd48fc6fbff72105249d1c8255e
portiaportia <portiaplante@gmail.com>
2026-03-17 09:37:11 -0400

    wefa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import "../css/Header.css";
+import Navigation from "./Navigation";
 
 const Header = () => {
     return (
         <header id="main-header">
             <h1>Finding A Home</h1>
+            <Navigation />
         </header>
     );
 };
