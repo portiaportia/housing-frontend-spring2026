a68d5d3a28f613684c5099626ae20eab178800f6
portiaportia <portiaplante@gmail.com>
2026-03-19 11:03:33 -0400

    udpated

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,6 +1,14 @@
 import "../css/Slideshow.css";
 import CountryHouse from "../images/slideshow/country-house.jpg";
 const Slideshow = () => {
+    const importAll = (resource) => {
+        return resource.keys().map(resource);
+    };
+
+    const images = importAll(
+        require.context("../images/slideshow", false, /\.(png|jpe?g|svg$|webp)/)
+    );
+
     return (
         <section className="slideshow">
             <img src={CountryHouse} />
