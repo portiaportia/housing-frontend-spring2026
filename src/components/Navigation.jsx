4fbeb6bdcf38d37a84e9efbbd05c03fb9c7858c3
portiaportia <portiaplante@gmail.com>
2026-03-24 08:58:38 -0400

    awer

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,21 @@
 import {Link} from "react-router-dom";
+import {useState} from "react";
 import "../css/Navigation.css";
 
 const Navigation = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+        console.log(menuOpen);
+    };
+
     return (
         <nav id="main-nav">
-            <ul className="columns">
+            <a id="toggle-nav" onClick={toggleMenu} href="#">
+                {menuOpen?(<p>&#8963;</p>):(<p>&#8964;</p>)}
+                </a>
+            <ul className={menuOpen?"columns":"hide-small"}>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/listings">Listings</Link></li>
