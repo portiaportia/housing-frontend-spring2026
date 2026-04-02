f235a7c43fda1b4bd74da185626ff28c851fb87f
portiaportia <portiaplante@gmail.com>
2026-04-02 09:08:14 -0400

    basic get

--- src/components/House.jsx
+++ src/components/House.jsx
@@ -1,11 +1,10 @@
 import "../css/House.css";
-import housePic from "../images/house.webp";
 
 const House = (props) => {
     return (
         <section className="house">
             <h3>{props.title}</h3>
-            <img src={housePic} />
+            <img src={`http://localhost:3001/images/${props.main_image}`} />
         </section>
     )
 };
--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -1,19 +1,31 @@
+import {useState, useEffect} from "react";
+import axios from "axios";
 import House from "../components/House";
 
 const Listings = () => {
+    const [houses, setHouses] = useState([]);
+
+    //after the page has loaded
+    useEffect(()=>{
+        const loadHouses = async() => {
+            const response = await axios.get("http://localhost:3001/api/houses");
+            setHouses(response.data);
+        };
+
+        loadHouses();
+    },[]);
+
     return (
         <main id="listings" className="main-content">
             <h2>Listings</h2>
             <div id="houses" className="columns">
-                <House 
-                    title="Cabin"
-                    description="Outdoor getaway" />
-                <House 
-                    title="Farm House"
-                    description="Chickens..."/>
-                <House 
-                    title="Modern Home"
-                    description="Lots of corners" />
+                {houses.map((house)=>(
+                    <House 
+                        key={house._id}
+                        _id={house._id}
+                        title={house.name}
+                        main_image={house.main_image} />
+                ))}
             </div>
         </main>
     );
