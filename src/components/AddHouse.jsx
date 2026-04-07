cf3845f0f61db84c3852a67b9c379dbbd5e20243
portiaportia <portiaplante@gmail.com>
2026-04-07 08:49:11 -0400

    stripped dialog

--- src/components/AddHouse.jsx
+++ src/components/AddHouse.jsx
@@ -2,7 +2,21 @@ import "../css/Dialog.css";
 
 const AddHouse = () => {
     return (
-        <h1>AddHOuse</h1>
+        <div id="house-dialog" className="w3-modal">
+            <div className="w3-modal-content">
+                <div className="w3-container">
+                    <span
+                        id="dialog-close"
+                        className="w3-button w3-display-topright"
+                    >
+                        &times;
+                    </span>
+                    <div id="house-dialog-content">
+                        <h1>Add HOuse</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
